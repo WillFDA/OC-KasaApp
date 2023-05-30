f3dcc5c449df3fa508b4e1d5de7af36f8d776aef
William <william.deazevedopro@gmail.com>
2023-05-30 13:08:37 +0200

    modif counter + scss

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -6,29 +6,38 @@ import { ReactComponent as LefttArrow } from '../../assets/leftarrow.svg';
 export default function ImageSlider(props) {
     const [currentIndex, setCurrentIndex] = useState(0);
     const location = props.location;
+    const [count, setCounter] = useState(1);
     useEffect(() => {
         if(currentIndex < 0) {
             setCurrentIndex(location.pictures.length - 1);
+            setCounter(location.pictures.length);
         } else if (currentIndex >= location.pictures.length) {
-            setCurrentIndex(0)
+            setCurrentIndex(0);
+            setCounter(1);
         }
     }, [currentIndex, location.pictures.length])
 
     function handlePreviousClick() {
         setCurrentIndex(currentIndex - 1)
+        setCounter(count - 1)
     }
 
     function handleNextClick() {
         setCurrentIndex(currentIndex + 1)
+        setCounter(count + 1)
     }
 
     return (
         <div className="slider">
-            <div className="slider__arrows">
-                <LefttArrow onClick={handlePreviousClick} className="slider--left" />
-                <RightArrow  onClick={handleNextClick} className="slider--right"/>
-            </div>  
-            <img src={location.pictures[currentIndex]} alt='`Photo ${currentIndex - 1}`' className="slider__img" />
+            {location.pictures.length > 1 && (
+                <div className="slider__arrows">
+
+                    <LefttArrow onClick={handlePreviousClick} className="slider--left" />
+                    <p className="slider--counter">{`${count}/${location.pictures.length}`}</p>
+                    <RightArrow  onClick={handleNextClick} className="slider--right"/>
+                </div>  
+            )}
+            <img src={location.pictures[currentIndex]} alt={location.title} className="slider__img" />
         </div>
     )
 }
