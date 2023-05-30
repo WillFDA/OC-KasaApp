53bfc15e1177800d01030ceb5261ea2efe49aede
William <william.deazevedopro@gmail.com>
2023-05-30 11:43:45 +0200

    h1 hero banner render only if true

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -4,7 +4,7 @@ import './_herobanner.scss'
 export default function HeroBanner(props) {
     return(
     <div className={props.classes.container}>
-        <h1 className='hero__title'>{props.text}</h1>
+      {props.text && <h1 className='hero__title'>{props.text}</h1>} 
         <img src={props.image} alt="Home" className={props.classes.img} />
       </div>
     )
